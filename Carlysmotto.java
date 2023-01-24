49dd80f64ab8ba2abb8a0c34f8dc51803bb4770c
mariell-hope <103894942+mariell-hope@users.noreply.github.com>
2023-01-24 21:11:35 +0100

    Carlysmotto.java
    Java task 2

---
+++ Carlysmotto.java
@@ -0,0 +1,7 @@
+public class CarlysMotto2 {
+    public static void main(String[] args) {
+        System.out.println("*********************************************");
+        System.out.println("*Carly's makes the food that makes the party*");
+        System.out.println("*********************************************");
+    }
+}
