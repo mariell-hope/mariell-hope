179c1a7d66cd8b4f776b1bb52a1ca0c3b58d295d
mariell-hope <mariell-ho@outlook.com>
2024-02-29 13:08:35 +0100

    Update Carlysmotto.java

--- Carlysmotto.java
+++ Carlysmotto.java
@@ -3,5 +3,6 @@ public class CarlysMotto2 {
         System.out.println("*********************************************");
         System.out.println("*Carly's makes the food that makes the party*");
         System.out.println("*********************************************");
+        //This is my comment to understand how github works
     }
 }
